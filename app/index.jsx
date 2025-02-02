dccfb74711b02e69a40cb23bad108c433e85d902
charan <sravanamcharan20@gmail.com>
2025-02-02 18:56:12 +0530

    Added Constants and assets

--- app/index.jsx
+++ app/index.jsx
@@ -6,8 +6,8 @@ export default function App() {
   return (
     <View className="flex-1 bg-white items-center justify-center">
       <Text className="text-red-500">Open up App.js to start working on your app!</Text>
-      <Link href='/profile'>
-        <Text>Go to profile</Text>
+      <Link href="/profile">
+        <Text className="text-3xl">Go to profile</Text>
       </Link>
       <StatusBar style="auto" />
     </View>
