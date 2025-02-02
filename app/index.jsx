0777c6d6c4474b9989fecfd597c7b5df3341f28f
charan <sravanamcharan20@gmail.com>
2025-02-02 19:05:35 +0530

    integrated Tailwind and fonts

--- app/index.jsx
+++ app/index.jsx
@@ -2,12 +2,13 @@ import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View className="flex-1 bg-white items-center justify-center">
-      <Text className="text-red-500">Open up App.js to start working on your app!</Text>
+      <Text className="text-black">Open up App.js to start working on your app!</Text>
       <Link href="/profile">
-        <Text className="text-3xl">Go to profile</Text>
+        <Text className="text-xl font-pbold">Go to profile</Text>
       </Link>
       <StatusBar style="auto" />
     </View>
